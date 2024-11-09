ea503304bb51531e6acd7b53589b86803758942d
Anna Klimova <aklimova@networkoptix.com>
2024-11-09 23:16:34 +0100

    Fix map issues and add icons state change on click

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Authenticator } from "@aws-amplify/ui-react";
 import App from "./App.tsx";
 import "./index.css";
-import { Amplify } from "aws-amplify";
+import {Amplify} from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 import '@aws-amplify/ui-react/styles.css'
 
@@ -11,8 +10,6 @@ Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Authenticator>
       <App />
-    </Authenticator>
   </React.StrictMode>
 );
